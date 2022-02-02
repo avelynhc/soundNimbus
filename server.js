f6d8f2d40eb95090fa1c6e25ca70e8d7da7e7f47
avelynhc <avelynhc@gmail.com>
2022-02-02 14:07:19 -0500

    moduele with promise to get back album data json

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const { traceDeprecation } = require("process")
+const musicData = require("./musicData")
 
 const HTTP_PORT = process.env.port || 8080
 const onHttpStart = () => console.log(`Express server is listening on port ${HTTP_PORT}`)
@@ -16,6 +18,47 @@ app.get("/", (req, res) => {
 app.get("/home", (req, res) => {
     // res.send("Hello Home!")
     res.sendFile(path.join(__dirname, "/views/index.html"))
+})
+
+app.get("/about", (req, res) => {
+    res.send("Hello about!")
+})
+
+app.get("/lyrics", (req, res) => {
+    // res.send("Hello lyrics!")
+    musicData.getalbums().then((data) => {
+        res.send(data)
+        // or res.json(data)
+    }).catch((error) => {
+        console.log(error)
+        res.status(404).send("ERROR!")
+    })
+})
+
+app.get("/lyrics/:id", (req, res) => {
+    // res.send("Hello lyrics!")
+    musicData.getalbums().then((data) => {
+        // res.send(data)
+        // or res.json(data)
+        // res.send(req.params.id) // this id comes from endpoint id
+        // res.send(data[0]) // access first item in the array
+        res.json(data[req.params.id-1])
+        res.json(data[req.params.id-1].lyrics)
+
+
+
+    }).catch((error) => {
+        console.log(error)
+        res.status(404).send("ERROR!")
+    })
+})
+
+app.get("/music", (req, res) => {
+    res.send("Hello music!")
+})
+
+app.use((req, res) => {
+    res.status(404).send("PAE NOT FOUND")
 })
 
 app.listen(HTTP_PORT, onHttpStart)
