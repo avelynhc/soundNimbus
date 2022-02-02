5624a64d9f797ae03209fabbe005e2c36947c0e7
avelynhc <avelynhc@gmail.com>
2022-02-02 13:02:09 -0500

    updated codes

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+
+const HTTP_PORT = process.env.port || 8080
+const onHttpStart = () => console.log(`Express server is listening on port ${HTTP_PORT}`)
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    // res.send("SoundNimbus")
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+})
+
+
+app.listen(HTTP_PORT, onHttpStart)
